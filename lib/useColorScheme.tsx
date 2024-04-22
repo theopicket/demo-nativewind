7e9ebe84b722c4c1d39e0fbbb595a4de6e59b575
theopicket <thanh.dao@pickethomes.com>
2024-04-23 00:12:46 +0700

    Initial commit
    Generated by create-expo-stack 2.0.0.

---
+++ lib/useColorScheme.tsx
@@ -0,0 +1,14 @@
+import { useColorScheme as useNativewindColorScheme } from 'nativewind';
+
+import { COLORS } from '~/theme/colors';
+
+export function useColorScheme() {
+  const { colorScheme, setColorScheme, toggleColorScheme } = useNativewindColorScheme();
+  return {
+    colorScheme: colorScheme ?? 'light',
+    isDarkColorScheme: colorScheme === 'dark',
+    setColorScheme,
+    toggleColorScheme,
+    colors: COLORS[colorScheme ?? 'light'],
+  };
+}
